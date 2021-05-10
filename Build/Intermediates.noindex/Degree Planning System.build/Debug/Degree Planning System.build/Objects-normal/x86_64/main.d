0afe1cfc86cdc0650663d4aa23a1216bd2c804a6
Taj Coleman <32987282+Drane2@users.noreply.github.com>
2021-05-09 21:21:02 -0500

    Added a few things
    Added a list of degree plans, along with a showMenu() function that would make printing the menus easier.
    
    Also added readFile() so the user can display .txt files, and some global variables

---
+++ Build/Intermediates.noindex/Degree Planning System.build/Debug/Degree Planning System.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/tajcoleman/Library/Mobile\ Documents/com~apple~CloudDocs/School/Programming\ III/Degree\ Planning\ System/Degree\ Planning\ System/main.cpp
--- Degree Planning System/main.cpp
+++ Degree Planning System/main.cpp
@@ -9,14 +9,69 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
-string newl = "\n";            // for newlines
-string typenum = "\n> ";       // prompt for choosing a feature
+// Global variables
+string newl = "\n"; // for newlines
+string typenum = "\n>  Enter a number: "; // prompt for choosing a feature
+string border = "\n--------------------------------------------------------------------\n";
 
+// List of options in the main menu
+vector<string> menu = {"Student Records: Edit and display student info",
+                       "Courses: Edit and display course info",
+                       "Degree Plan: Edit and display degree plan",
+                       "Semester Schedules: Edit and display semester info",
+                       "Exit: Close the program"};
+// List of degree plans
+vector<string> degreePlans = {"CIT, Desktop Support and Microsoft Network Administration",
+                            "CIT, Information Technology Cyber Security Specialty",
+                            "CIT, Introductory Game Design and Development",
+                            "CIT, Network Administration Cisco Specialty",
+                            "COMPUTER INFORMATION TECHNOLOGY - NETWORKING",
+                            "COMPUTER INFORMATION TECHNOLOGY - PROGRAMMING"};
+
+// Edit or Display
+vector<string> editDisplay = {"Edit", "Display"};
+
+// To display the files
+void readFile(string fileName)
+{
+    string line;
+    ifstream myfile(fileName);
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            cout << line << "\n";
+        }
+        myfile.close();
+    }
+    else cout << "\n> Error: file not open\n" << endl;
+}
+
+// To display a menu with options and a heading
+static void showMenu(char end, string heading, vector<string> list)
+{
+    cout << border;
+    cout << setw(0) << " " << heading;
+    cout << border << newl;
+    
+    for(int i=0;i<list.size();i++)
+    {
+        auto menu_option = list[i].substr(0, list[i].find(end));
+        cout << i+1 << ": " << menu_option << "\n\n";
+    }
+}
+
+// To navigate the menu
 void chooseOption(int x, int y)
 {
+    // Variables
+    int z;
+
     if(x==1)
     {
         if(y==1)
@@ -25,7 +80,7 @@ void chooseOption(int x, int y)
         }
         else if(y==2)
         {
-            // showStudents();
+            readFile("students.txt");
         }
     }
     else if(x==2)
@@ -36,7 +91,7 @@ void chooseOption(int x, int y)
         }
         else if(y==2)
         {
-            // showCourses();
+            readFile("students.txt");
         }
     }
     else if(x==3)
@@ -47,7 +102,13 @@ void chooseOption(int x, int y)
         }
         else if(y==2)
         {
-            // showDegreePlan();
+            showMenu('\n', "Choose a Degree Plan you'd like to display", degreePlans);
+            
+            cout << typenum;
+            cin >> z;
+
+            string txtfile = degreePlans[z-1] + ".txt";
+            readFile(txtfile);
         }
     }
     else if(x==4)
@@ -71,28 +132,16 @@ int main()
 {
   // Declare variables
   int x, y;
-  vector<string> menu = {"Students: Edit and display student info",
-                         "Courses: Edit and display course info",
-                         "Degree Plan: Edit and display degree plan",
-                         "Semester: Edit and display semester info",
-                         "Exit: Close the program"};
 
   // List the options in the menu
-  cout << "\nWelcome to the Degree Planning System\n\n";
-  for(int i=0;i<menu.size();i++)
-  {
-    auto menu_option = menu[i].substr(0, menu[i].find(':')); // So it doesn't print the
-    cout << i+1 << ") " << menu_option << newl;              // descripttion
-  }
+    showMenu(':', "Degree Planning System", menu);
 
   // Ask user to choose
   cout << typenum;
   cin >> x;
   
-  // Call functions
-  cout << newl << menu[x-1] << newl;
-  cout << "\n1) Edit";
-  cout << "\n2) Display\n";
+  // Call Functions
+  showMenu('\n', menu[x-1], editDisplay);
   cout << typenum;
 
   cin >> y;
